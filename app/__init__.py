84f2da5cdb0d508ec66466bef1c14b35569c8338
kataphasis <kataphasis22@gmail.com>
2022-07-12 02:40:53 -0600

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+
+    @app.route("/about")
+    def about():
+        return render_template('about.html')
+
+    @app.route("/login")
+    def contact():
+        return render_template('login.html')
+
+    @app.route("/register")
+    def contact():
+        return render_template('register.html')
+
+    @app.route("/blog")
+    def blog():
+        return render_template('blog.html')
+    return app
